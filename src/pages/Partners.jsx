2d812a13928528ea7b5984d44c82df662c1a6acc
thawzinmoemyint <thawzinmoemyint@thompsons-MacBook-Pro.local>
2024-04-27 23:19:14 +0700

    add underline under headers

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -5,10 +5,12 @@ import partner2 from "../components/assets/images/beam.jpg"
 export const Partners = () => {
   return (
     <>
-    <section className="partners mb-16">
+    <section className="partners py-16" sytle={{paddingBottom: "20px"}}>
       <div className="container">
         <div className="heading py-12 text-center w-2/3 m-auto md:w-full">
-          <h1 className="text-3xl font-semibold text-black">Educational Partners</h1>
+          <h1 className="text-3xl font-semibold text-black">
+            <span className="border-b-2 border-black pb-2">Educational Partners</span>
+            </h1>
         </div>
         <div className="partners-container flex justify-center gap-8">
           <div className="partner-item">
